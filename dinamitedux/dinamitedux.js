755d6a2cee4c846798e2bdf55f6208c499634a04
Eduardo Ottaviani <Edu@MacBook-Pro.local>
2016-05-22 01:22:47 -0300

    Fixed dinamitedux scope

--- dinamitedux/dinamitedux.js
+++ dinamitedux/dinamitedux.js
@@ -2,13 +2,15 @@ define(function(){
 
 	return function( pubsub, state ){
 
+		var self = this;
+
 		this.getState = function(){
 			return Object.assign( state )
 		};
 
 		pubsub.subscribe('store', function( payload ){
-			if( payload.action in this ){
-				var newstate = this[ payload.action ].call( null, state, payload )
+			if( payload.action in self ){
+				var newstate = self[ payload.action ].call( null, state, payload )
 				pubsub.publish( 'store:update', newstate )
 			}
 		})
