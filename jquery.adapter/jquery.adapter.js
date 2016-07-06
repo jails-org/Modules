f793d353d7fcd863e81c25f92c3f8c5f4e4924f2
Eduardo Ottaviani Aragão <edudu_tata@hotmail.com>
2016-07-06 19:42:59 -0300

    Update jquery.adapter.js

--- jquery.adapter/jquery.adapter.js
+++ jquery.adapter/jquery.adapter.js
@@ -1,18 +1,18 @@
-define(function($){
-
-	return{
-		on :function(el, ev, callback){
-			$(el).on(ev, function(e, data){
-				e.detail = data? data.detail :e.detail;
-				return callback.call(this, e);
-			});
-		},
-		off:function(el, ev, callback){
-			$(el).off(ev);
-		},
-		trigger :function(el, ev, args){
-			$(el).trigger(ev, {detail:args} );
-		}
+define(function(){
+	return function($){
+		return{
+			on :function(el, ev, callback){
+				$(el).on(ev, function(e, data){
+					e.detail = data? data.detail :e.detail;
+					return callback.call(this, e);
+				});
+			},
+			off:function(el, ev, callback){
+				$(el).off(ev);
+			},
+			trigger :function(el, ev, args){
+				$(el).trigger(ev, {detail:args} );
+			}
+		};
 	};
-
 });
