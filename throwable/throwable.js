c6137956ab0040c5b33370901d45558ff7ed027b
Eduardo Ottaviani <Edu@MacBook-Pro.local>
2016-08-05 00:44:47 -0300

    New throwable for jails 2

--- throwable/throwable.js
+++ throwable/throwable.js
@@ -1,114 +1,46 @@
 define(['jails'],function( jails ){
 
-	return function(config){
+	return function(){
 
-		config = config || {};
-		var debug = config.debug || false;
+		var components = jails.components;
+		var component  = jails.Component.prototype;
 
-		wrap();
-		jails.events.on( window, 'error', error );
+		for ( var name in components ){
+			components[name] = wrap( name, components[name] );
+		}
 
-		function wrap(){
-
-			var module, item,
-			modules = ['app', 'controller'],
-			len = modules.length;
-
-			for(var i = 0; i < len; i++){
-
-				module = modules[i];
-				item = jails[module+'s'];
-
-				for( var name in item ){
-					item[name] = wrapper( item[name], module );
-				}
+		for( var method in component ){
+			if( component[method].call && method != 'subscribe' && method != 'publish' ){
+				component[method] = wrap( method, component[method] );
 			}
-		}
-
-		function wrapper( fn, type ){
-
-			return function( html, data ){
-
-				var instance = this;
-
-				if(debug) fn.apply( instance, arguments );
-				else try{
-					fn.apply( instance, arguments );
-				}
-				catch(err){
-					error({ error :{
-						at 		:type,
-						message :err,
-						element :html,
-						module	:instance,
-						data 	:data
-					}});
-				}
-
-				if(!debug)
-				productionize( instance, {
-					at 		:type,
-					message :'',
-					element :html,
-					module	:instance,
-					data 	:data
-				});
-
-				instance.try = function(fn){
-					return debug? fn :function(){
-						try{ fn.apply( this, arguments ); }
-						catch(err){
-							error({ error :{
-								at 		:type,
-								message :err,
-								element :html,
-								module	:instance,
-								data 	:data
-							}});
-						}
-					};
-				};
-			};
-		}
-
-		function productionize( object, thrown ){
-
-			var name, method;
-
-			for (name in object){
-				method = object[name];
-
-				if( method.call ){
-					object[name] = function( name, method ){
-						return function(){
-							try {
-								return method.apply(this, arguments);
-							} catch (err) {
-								thrown.message = err;
-								error({ error : thrown });
-							}
-						};
-					}(name, method);
-				}
-			}
-		}
-
-		function error(e){
-
-			if( debug ) return false;
-
-			var err = e.error;
-
-			jails.publish('throwable', err, e);
-
-			if( err.at )
-				jails.publish('throwable@'+err.at+':'+err.module.name, err, e);
-
-			if( e.preventDefault )
-				e.preventDefault();
-
-			return true;
 		}
 	};
+
+	function wrap( name, fn ){
+
+		return function(){
+
+			try{
+				var cp = fn.apply( null, arguments );
+
+				if( cp ){
+					for (var method in cp ){
+						if( cp[method].call && method != 'subscribe' && method != 'publish' ){
+							cp[method] = wrap(name, cp[method])
+						}
+					}
+				}
+
+				return cp || {};
+
+			}catch( err ){
+
+				jails.publish('throwable', { err: err, name :name });
+				jails.publish('throwable@' + name, { err: err, name :name });
+
+				return {};
+			}
+		};
+	}
 
 });
