afab28869dbd68accb8bddf4cfa1718352f517c8
Eduardo Ottaviani Aragão <earagao@fbiz.com.br>
2017-01-25 16:37:37 -0200

    New logger for jails 2.2.x version

--- logger/logger.js
+++ logger/logger.js
@@ -1,104 +1,43 @@
-define(['jails'],function( jails ){
+define(function(){
 
-	var root 	   = document;
-	var Component  = jails.Component;
-	var components = jails.components;
+	return function( jails ){
 
-	//1. Html markup not found
-	function markup( target ){
+		var base = jails.component;
+		var root = document.documentElement;
 
-		for( var name in components ){
-			name = name.replace(/\//g, '\\/');
-			if( !(target||root).querySelector('[data-component*='+name+']' ) ){
-				console.warn(
-					style( 'Component.{0} was not found on html markup.', name )
-				);
-			}
-		}
-	}
+		log();
 
-	//2. Html markup found but not the module
-	function module_not_found( target ){
+		jails.component = function( name, element ){
 
-		var m = (target||root).querySelectorAll('[data-component]');
+			var component;
 
-		for( var c = 0; c < m.length; c++){
-			var name = m[c].getAttribute('data-component').replace(/\,/, ' ').replace(/\s{2,}/, ' ').split(/\s/);
+			module_not_loaded( name, jails.components );
+			module_not_found( jails.components );
 
-			for(var x = 0; x < name.length; x++)
-				if( !(name[x] in jails.components) ){
-					console.warn(
-						style( 'Component.{0} is referenced on markup, but module is not loaded into Jails yet.', name[x] )
-					);
-				}
-		}
-
-	}
-
-	//3. Watch for emit and listening
-	function emit_and_listen(){
-
-		var proto = Component.prototype;
-
-		var base = {
-			emit 	:proto.emit,
-			listen 	:proto.listen,
-			get 	:proto.get,
-			publish :proto.publish,
-			subscribe :proto.subscribe
+			component = base.apply( this, arguments );
+			return component;
 		};
 
-		proto.emit = function(ev, args){
-			console.log( style( '%cComponent.{0} emits \'{0}:{1}\'', this.name, ev ), 'color:green;', args );
-			base.emit.apply( this, arguments );
-		}
-
-		proto.listen = function(ev, args){
-			var name = this.name, ret;
-			console.log( style( 'Component.{0} is listening to %c\'{1}\'', name, ev ), 'color:#336699;' );
-			ret = base.listen.apply( this, arguments );
-			jails.events.on(this.element, ev, function(){
-				console.log( style( 'Component.{0} listened to %c\'{1}\'', name, ev ), 'color:green;' );
-			});
-			return ret;
-		}
-
-		proto.get = function( target ){
-			var _self = this;
-			return function(method){
-				var args = Array.prototype.slice.call(arguments);
-				args.shift();
-				console.log( style( '[{0}] => '+target+'.%c'+method +' ✓', _self.name, target ), 'color:#336699', args );
-				return base.get.call(_self, target).apply(_self, arguments);
+		function empty( name ){
+			return function( c, el, props ){
+				console.info('%c||\\| %c[ Logger %c] - [%c '+name+' %c] is not loaded on jails.start().', 'color:#369BBF; font-weight:bolder; padding:5px 0;', 'font-weight:bold', 'font-weight:normal', 'font-weight:bold; color:#369BBF', 'font-weight:normal');
 			};
 		}
 
-		proto.publish = function(ev, args){
-			console.log( style( '[{0}] %cpublished \'{1}\' ✓', this.name || 'jails', ev ), 'color:green;', args );
-			base.publish.apply( this, arguments );
+		function module_not_loaded( name, components ){
+			if( !(name in components) )
+				components[name] = empty(name);
 		}
 
-		proto.subscribe = function(ev, args){
-			console.log( style( '[{0}] %csubscribed to \'{1}\' ✓', this.name || 'jails', ev ), 'color:green;' );
-			base.subscribe.apply( this, arguments );
-		}
-	}
-
-	function style( string ){
-
-		for (var i = 1; i < arguments.length; i++){
-			string = string.replace( new RegExp('\\{' + (i-1) + '\\}', 'g'), arguments[i] );
+		function module_not_found( components ){
+			for(var name in components)
+				if( !document.querySelector('[data-component*='+name+']') )
+					console.info('%c||\\| %c[ Logger %c] - [%c '+name+' %c] not found in markup on jails.start().', 'color:#369BBF; font-weight:bolder; padding:5px 0;', 'font-weight:bold', 'font-weight:normal', 'font-weight:bold; color:#369BBF', 'font-weight:normal');
 		}
 
-		return '👓 [Jails::logger] 👉 ' + string;
-	}
-
-	return function( target ){
-		console.log( '👓%c[ Welcome to Jails Logger ]👓', 'color:#336699');
-
-		markup( target );
-		module_not_found( target );
-		emit_and_listen();
-	}
+		function log(){
+			console.log('%c||\\| %c[ Logger %c] is ready.', 'color:#369BBF; font-weight:bolder; padding:5px 0;', 'font-weight:bold', 'font-weight:normal');
+		}
+	};
 
 });
